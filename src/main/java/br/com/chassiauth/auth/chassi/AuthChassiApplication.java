5c465e00575c6cbad043e6d0ccce9eaf3b127447
igorbavand <igor.bvn@gmail.com>
2023-03-04 02:20:59 -0300

    security

---
+++ src/main/java/br/com/chassiauth/auth/chassi/AuthChassiApplication.java
@@ -0,0 +1,13 @@
+package br.com.chassiauth.auth.chassi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthChassiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthChassiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chassiauth/auth/chassi/AuthChassiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chassiauth.auth.chassi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthChassiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
